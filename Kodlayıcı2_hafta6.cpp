c54b5e6c9ec5b4043b46c0f33760b128f2e330c3
azradudu4251 <148636146+azradudu4251@users.noreply.github.com>
2023-11-19 12:52:07 +0300

    Hafta 6 Kodlayıcı 2

---
+++ Kodlayıcı2_hafta6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int ogrenci_no=5;ogrenci_no<25;ogrenci_no++)
+    {
+        if(ogrenci_no%3==0)
+            cout <<"Ogrenci no:"<< ogrenci_no << endl;
+    }
+}
