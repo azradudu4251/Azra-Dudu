0c73a7c58cbab94fec31a5bb8881107ea80dd12f
azradudu4251 <148636146+azradudu4251@users.noreply.github.com>
2023-12-01 17:41:53 +0300

    7. hafta tasarlayıcı ödevi

--- ödev.cpp
+++ ödev.cpp
@@ -1,3 +1,18 @@
-cin >> yas1
-cout << "2. kisinin yasini giriniz: "
-cout << "2 kisinin yasinin toplami: " << (yas1+yas2)
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int NOT[12] = {97,78,72,61,50,67,80,95,59,100,100,83};
+    int toplam = 0, A = 12;
+    float ortalama;
+    for (int b=0; b<A; b++)
+    {
+        cout <<"Arkadasimin notu:" << NOT[b] << endl;
+        toplam += NOT[b];
+    }
+    ortalama=toplam/A;
+    cout << "Arkadaslarimin notlarinin ortalamasi:"<< ortalama;
+    return 0;
+}
